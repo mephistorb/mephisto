0ae997df0aefce9ebdbf7f290f0c27eb6b63878b
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2005-12-29 02:12:56 +0000

    added external libs and schema
    git-svn-id: http://svn.techno-weenie.net/projects/mephisto@513 567b1171-46fb-0310-a4c9-b4bef9110e78

--- config/environment.rb
+++ config/environment.rb
@@ -32,11 +32,11 @@ Rails::Initializer.run do |config|
   # config.active_record.observers = :cacher, :garbage_collector
 
   # Make Active Record use UTC-base instead of local time
-  # config.active_record.default_timezone = :utc
+  config.active_record.default_timezone = :utc
   
   # Use Active Record's schema dumper instead of SQL when creating the test database
   # (enables use of different database adapters for development and test environments)
-  # config.active_record.schema_format = :ruby
+  config.active_record.schema_format = :ruby
 
   # See Rails::Configuration for more options
 end
---
+++ lib/time_ext.rb
@@ -0,0 +1,20 @@
+class Time
+  def to_delta(delta_type = :day)
+    case delta_type
+      when :year then self.class.delta(year)
+      when :month then self.class.delta(year, month)
+      else self.class.delta(year, month, day)
+    end
+  end
+      
+  # Borrowed from Typo
+  def self.delta(year, month = nil, day = nil)
+    from = Time.mktime(year, month || 1, day || 1)
+
+    to   = from + 1.year
+    to   = from + 1.month unless month.blank?    
+    to   = from + 1.day   unless day.blank?
+    to   = to.tomorrow    unless month.blank?
+    return [from, to]
+  end
+end
