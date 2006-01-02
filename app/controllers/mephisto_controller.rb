26356867c172a0286353979e14301c8a60addc7f
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2006-01-02 17:04:29 +0000

    anal spacing
    git-svn-id: http://svn.techno-weenie.net/projects/mephisto@552 567b1171-46fb-0310-a4c9-b4bef9110e78

--- app/controllers/mephisto_controller.rb
+++ app/controllers/mephisto_controller.rb
@@ -12,25 +12,25 @@ class MephistoController < ApplicationController
     end
 
     @article_pages = Paginator.new self, @tag.articles.size, 15, params[:page]
-    @articles = @tag.articles.find_by_date(
-                  :limit  =>  @article_pages.items_per_page,
-                  :offset =>  @article_pages.current.offset).collect { |a| a.to_liquid }
+    @articles      = @tag.articles.find_by_date(
+                       :limit  =>  @article_pages.items_per_page,
+                       :offset =>  @article_pages.current.offset).collect { |a| a.to_liquid }
 
     render_liquid_template_for(template_type, 'tag' => @tag, 'articles' => @articles)
   end
 
   def search
-    conditions = ['title LIKE :q OR summary LIKE :q OR description LIKE :q', { :q => "%#{params[:q]}%" }]
+    conditions     = ['title LIKE :q OR summary LIKE :q OR description LIKE :q', { :q => "%#{params[:q]}%" }]
     @article_pages = Paginator.new self, Article.count(conditions), 15, params[:page]
-    @articles = Article.find(:all, :conditions => conditions, :order => 'published_at DESC',
-                  :limit  =>  @article_pages.items_per_page,
-                  :offset =>  @article_pages.current.offset).collect { |a| a.to_liquid }
+    @articles      = Article.find(:all, :conditions => conditions, :order => 'published_at DESC',
+                       :limit  =>  @article_pages.items_per_page,
+                       :offset =>  @article_pages.current.offset).collect { |a| a.to_liquid }
 
     render_liquid_template_for(:search, 'tag' => @tag, 'articles' => @articles)
   end
 
   def show
-    @article = Article.find_by_permalink(params[:year], params[:month], params[:day], params[:permalink])
+    @article  = Article.find_by_permalink(params[:year], params[:month], params[:day], params[:permalink])
     @comments = @article.comments.collect { |c| c.to_liquid }
     render_liquid_template_for(:single, 'articles' => [@article.to_liquid], 'comments' => @comments)
   end
@@ -38,9 +38,9 @@ class MephistoController < ApplicationController
   protected
   def render_liquid_template_for(template_type, assigns = {})
     headers["Content-Type"] ||= 'text/html; charset=utf-8'
-    templates          = Template.templates_for(template_type)
-    preferred_template = Template.find_preferred(template_type, templates)
-    layout_template    = templates['layout']
+    templates                 = Template.templates_for(template_type)
+    preferred_template        = Template.find_preferred(template_type, templates)
+    layout_template           = templates['layout']
     assigns.merge! 'content_for_layout' => Liquid::Template.parse(preferred_template).render(assigns)
     render :text => Liquid::Template.parse(layout_template).render(assigns)
   end
