17693292cdd0dc5543297da4dc099ca272a8e42c
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2006-01-03 04:38:11 +0000

    added default admin route
    git-svn-id: http://svn.techno-weenie.net/projects/mephisto@556 567b1171-46fb-0310-a4c9-b4bef9110e78

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
+  map.admin   'admin', :controller => 'admin/base', :action => 'index'
   map.connect ':controller/:action/:id'
   map.article ':year/:month/:day/:permalink', :controller => 'mephisto', :action => 'show'
   map.search  'search/:q', :controller => 'mephisto', :action => 'search', :q => nil
