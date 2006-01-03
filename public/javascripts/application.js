628563dda4e1614d0a108e3687bd46ae9633de11
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2006-01-03 05:02:50 +0000

    remove ajax debugger
    git-svn-id: http://svn.techno-weenie.net/projects/mephisto@559 567b1171-46fb-0310-a4c9-b4bef9110e78

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -30,16 +30,16 @@ var ArticleForm = {
   }
 }
 
-Ajax.Responders.register({
-  // log the beginning of the requests
-  onCreate: function(request, transport) {
-    new Insertion.Bottom('debug', '<p><strong>[' + new Date().toString() + '] accessing ' + request.url + '</strong></p>')
-  },
-  
-  // log the completion of the requests
-  onComplete: function(request, transport) {
-    new Insertion.Bottom('debug', 
-      '<p><strong>http status: ' + transport.status + '</strong></p>' +
-      '<pre>' + transport.responseText.escapeHTML() + '</pre>')
-  }
-});
+//Ajax.Responders.register({
+//  // log the beginning of the requests
+//  onCreate: function(request, transport) {
+//    new Insertion.Bottom('debug', '<p><strong>[' + new Date().toString() + '] accessing ' + request.url + '</strong></p>')
+//  },
+//  
+//  // log the completion of the requests
+//  onComplete: function(request, transport) {
+//    new Insertion.Bottom('debug', 
+//      '<p><strong>http status: ' + transport.status + '</strong></p>' +
+//      '<pre>' + transport.responseText.escapeHTML() + '</pre>')
+//  }
+//});
