f2009e5cd8877f600973d14e2da05fca95f35b28
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2006-01-30 21:20:08 +0000

    added new article sweeping
    git-svn-id: http://svn.techno-weenie.net/projects/mephisto@636 567b1171-46fb-0310-a4c9-b4bef9110e78

--- app/cachers/article_sweeper.rb
+++ app/cachers/article_sweeper.rb
@@ -1,6 +1,15 @@
 class ArticleSweeper < ActionController::Caching::Sweeper
   observe Article
 
+  def after_create(record)
+    controller.expire_page :tags => [], :controller => '/mephisto', :action => 'list'
+    controller.expire_page :tags => [], :controller => '/feed', :action => 'feed'
+    Tag.find(:all, :conditions => ['name != ?', 'home']).each do |tag|
+      controller.expire_page :tags => tag.name.split('/'), :controller => '/mephisto', :action => 'list'
+      controller.expire_page :tags => tag.name.split('/'), :controller => '/feed', :action => 'feed'
+    end
+  end
+
   def after_save(record)
     pages = CachedPage.find_by_reference(record)
     unless pages.empty?
