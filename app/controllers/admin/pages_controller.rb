a97b9aa46d6c639c0334f94109fb5fc331690ffc
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2006-02-01 20:46:15 +0000

    allow ordering of pages
    git-svn-id: http://svn.techno-weenie.net/projects/mephisto@660 567b1171-46fb-0310-a4c9-b4bef9110e78

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -1,12 +1,21 @@
 class Admin::PagesController < Admin::BaseController
   cache_sweeper :tag_sweeper
-  verify :params => :id, :only => [:edit, :update],
+  verify :params => :id, :only => [:edit, :update, :order],
          :add_flash   => { :error => 'Tag required' },
          :redirect_to => { :action => 'index' }
   verify :method => :post, :params => :tag, :only => :update,
          :add_flash   => { :error => 'Tag required' },
          :redirect_to => { :action => 'edit' }
   before_filter :select_tag, :except => :index
+
+  def order
+    Article.transaction do
+      params[:articles].each_with_index do |pos, index|
+        @tag.taggings.detect { |t| t.article_id.to_s == pos }.update_attributes(:position => index)
+      end
+    end
+    render :nothing => true
+  end
 
   def update
     saved = @tag.update_attributes(params[:tag])
