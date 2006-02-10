84d8cc5f4f90b0bc16406aafe6361905975f495d
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2006-02-10 20:36:49 +0000

    fixed view error for unpublished articles
    git-svn-id: http://svn.techno-weenie.net/projects/mephisto/trunk@736 567b1171-46fb-0310-a4c9-b4bef9110e78

--- app/helpers/admin/articles_helper.rb
+++ app/helpers/admin/articles_helper.rb
@@ -4,4 +4,14 @@ module Admin::ArticlesHelper
                        :pending     => 'flag_yellow',
                        :published   => 'flag_green' }
   end
+
+  def link_to_article(article)
+    article.published? ?
+      link_to(h(article.title), article_url(article.hash_for_permalink)) :
+      '<small>preview link</small>'
+  end
+
+  def published_at_for(article)
+    article.published? ? article.published_at.to_s(:long) : "not published"
+  end
 end
