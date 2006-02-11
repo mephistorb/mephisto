866030130a87fa48d38ac5c16cfc1cab6a5f7374
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2006-02-11 07:41:06 +0000

    added tests for site settings
    git-svn-id: http://svn.techno-weenie.net/projects/mephisto/trunk@754 567b1171-46fb-0310-a4c9-b4bef9110e78

--- test/functional/admin/settings_controller_test.rb
+++ test/functional/admin/settings_controller_test.rb
@@ -5,14 +5,23 @@ require 'admin/settings_controller'
 class Admin::SettingsController; def rescue_action(e) raise e end; end
 
 class Admin::SettingsControllerTest < Test::Unit::TestCase
+  fixtures :sites, :users
   def setup
     @controller = Admin::SettingsController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
+    login_as :quentin
   end
 
   # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_should_show_settings
+    get :index
+    assert_tag :tag => 'input', :attributes => { :id => 'current_site_title', :value => sites(:first).title }
+  end
+
+  def test_should_update_settings
+    post :update, :current_site => { :title => 'foo' }
+    assert_equal 'foo', sites(:first).title
+    assert_redirected_to :action => 'index'
   end
 end
