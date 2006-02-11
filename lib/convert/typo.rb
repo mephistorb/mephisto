0ebee237b50b8129889f3020b2265c367343eb63
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2006-02-11 00:52:31 +0000

    cleaned up convert scripts
    git-svn-id: http://svn.techno-weenie.net/projects/mephisto/trunk@741 567b1171-46fb-0310-a4c9-b4bef9110e78

--- lib/convert/typo.rb
+++ lib/convert/typo.rb
@@ -10,33 +10,40 @@ module Typo
     # migrate users over, sorta ...
     Typo::User.find(:all).each do |user|
       email = 'foo@bar.com' if user.email.nil?
-      u = ::User.create(:login => user.login, :email => email,
-                        :password => newpass, :password_confirmation => newpass)
+      u     = ::User.create \
+        :login                 => user.login, 
+        :email                 => email,
+        :password              => newpass,
+        :password_confirmation => newpass
     end
 
-    Typo::Article.find(:all, :conditions => [ 'type = ?', 'Article']).each do |article|
-      comments = Typo::Comment.find(:all, :conditions => [ 'article_id = ?', article.id ])
-      
-      user = article.user_id.nil? : ::User.find(:first) : ::User.find_by_login(Typo::User.find(article.user_id).login)
+    Typo::Article.find_all_by_type('Article').each do |article|
 
-      a = ::Article.create(:title        => article.title, 
-                           :excerpt      => article.body,
-                           :body  => article.extended,
-                           :created_at   => article.created_at,
-                           :published_at => article.created_at,
-                           :updated_at   => article.updated_at,
-                           :user_id      => user)
+      user = article.user_id.nil? ? 
+        ::User.find(:first) : 
+        ::User.find_by_login(Typo::User.find(article.user_id).login)
 
-      article.tags.each { |tag| a.categorizations.create :category => ::Category.find_or_create_by_name(tag.name) }
-      comments.each do |comment|
-        a.comments << ::Comment.create(:body  => comment.body,
-                             :created_at   => comment.created_at,
-                             :updated_at   => comment.updated_at,
-                             :published_at => comment.created_at,
-                             :author       => comment.author,
-                             :author_url   => comment.url,
-                             :author_email => comment.email,
-                             :author_ip    => comment.ip)
+      a = ::Article.create \
+        :title        => article.title, 
+        :excerpt      => article.body,
+        :body         => article.extended,
+        :created_at   => article.created_at,
+        :published_at => article.created_at,
+        :updated_at   => article.updated_at,
+        :user_id      => user
+
+      article.categories.each { |category| a.categorizations.create :category => ::Category.find_or_create_by_name(category.name) }
+
+      Typo::Comment.find_all_by_article_id(article.id).each do |comment|
+        a.comments << ::Comment.create \
+          :body         => comment.body,
+          :created_at   => comment.created_at,
+          :updated_at   => comment.updated_at,
+          :published_at => comment.created_at,
+          :author       => comment.author,
+          :author_url   => comment.url,
+          :author_email => comment.email,
+          :author_ip    => comment.ip
       end
     end
   end
