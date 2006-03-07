0049fb73bc043568ee1f79dff1c1fa5ca917971e
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2006-03-07 06:40:25 +0000

    show a little more info with assert_difference
    git-svn-id: http://svn.techno-weenie.net/projects/mephisto/trunk@972 567b1171-46fb-0310-a4c9-b4bef9110e78

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,8 +10,7 @@ class Test::Unit::TestCase
   def assert_difference(object, method = nil, difference = 1)
     initial_value = object.send(method)
     yield
-    assert_equal initial_value + difference,
-      object.send(method)
+    assert_equal initial_value + difference, object.send(method), "#{object}##{method}"
   end
 
   def assert_no_difference(object, method, &block)
