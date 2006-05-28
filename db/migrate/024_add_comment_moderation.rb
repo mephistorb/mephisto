73f56dbad03068d188cec2273fc19810c9dfb36f
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2006-05-28 02:36:29 +0000

    all current comments are approved
    git-svn-id: http://svn.techno-weenie.net/projects/mephisto/trunk@1152 567b1171-46fb-0310-a4c9-b4bef9110e78

--- db/migrate/024_add_comment_moderation.rb
+++ db/migrate/024_add_comment_moderation.rb
@@ -1,7 +1,12 @@
 class AddCommentModeration < ActiveRecord::Migration
+  class Content < ActiveRecord::Base
+  end
+  class Comment < Content
+  end
   def self.up
     add_column "contents", "approved", :boolean, :default => false
     add_column "content_versions", "approved", :boolean, :default => false
+    Comment.update_all ['approved=?', true]
   end
 
   def self.down
