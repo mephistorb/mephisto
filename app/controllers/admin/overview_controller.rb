1e1a945dbd184106a1da61f7aa090e313880fbba
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2006-06-07 06:22:23 +0000

    show articles with unmoderated comments on the overview sidebar
    git-svn-id: http://svn.techno-weenie.net/projects/mephisto/trunk@1190 567b1171-46fb-0310-a4c9-b4bef9110e78

--- app/controllers/admin/overview_controller.rb
+++ app/controllers/admin/overview_controller.rb
@@ -9,6 +9,8 @@ class Admin::OverviewController < Admin::BaseController
     @users = User.find(:all)
     @events, @todays_events, @yesterdays_events = [], [], []
     today, yesterday = Time.now.to_date, 1.day.ago.to_date
+    @articles = @site.articles.find(:all, :include => :all_comments, :conditions => ['all_comments_contents.id is not null and all_comments_contents.approved = ?', false])
+    @articles.sort! { |x,y| y.comments.size <=> y.comments.size }
     @site.events.find(:all, :order => 'events.created_at DESC', :include => [:article, :user], :limit => 50).each do |event|
       case event.created_at.to_date
         when today     then @todays_events
