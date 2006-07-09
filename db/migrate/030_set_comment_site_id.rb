29cabac5c498be59cc077a2a39b3bbd292d178d9
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2006-07-09 01:45:35 +0000

    make comment migration a little more fool-proof
    git-svn-id: http://svn.techno-weenie.net/projects/mephisto/trunk@1319 567b1171-46fb-0310-a4c9-b4bef9110e78

--- db/migrate/030_set_comment_site_id.rb
+++ db/migrate/030_set_comment_site_id.rb
@@ -6,9 +6,14 @@ class SetCommentSiteId < ActiveRecord::Migration
   def self.up
     comments = Comment.find(:all, :select => 'id, site_id, article_id')
     return unless comments.any?
-    articles = Article.find(:all, :select => 'id, site_id', :conditions => ['id in (?)', comments.collect(&:article_id).uniq]).inject({}) { |h, article| h.merge article.id => article }
+    articles = Article.find(:all, :select => 'id, site_id', :conditions => ['id in (?)', comments.collect(&:article_id).uniq]).inject({}) { |h, article| h.merge article.id.to_s => article }
     comments.each do |comment|
-      comment.update_attribute :site_id, articles[comment.article_id].site_id
+      article = articles[comment.article_id.to_s]
+      if article
+        comment.update_attribute :site_id, article.site_id
+      else
+        say "Comment ##{comment.id} has an invalid article ##{comment.article_id}"
+      end
     end
   end
 
