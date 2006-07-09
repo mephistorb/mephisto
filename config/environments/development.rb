80e4ed86c251c98f9f44687ca968d1156edf55e4
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2006-07-09 02:39:11 +0000

    enable the referenced page caching controller
    git-svn-id: http://svn.techno-weenie.net/projects/mephisto/trunk@1328 567b1171-46fb-0310-a4c9-b4bef9110e78

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,3 +17,8 @@ config.action_controller.perform_caching             = true
 
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
+
+config.after_initialize do
+  require 'application' unless Object.const_defined?(:ApplicationController)
+  ReferencedPageCachingController.enabled = true
+end
