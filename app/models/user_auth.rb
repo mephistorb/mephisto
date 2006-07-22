4ef042d0684bd0934eecf8104baa40bb983ebe8a
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2006-07-22 23:10:42 +0000

    extract user auth stuff into abstract base model
    git-svn-id: http://svn.techno-weenie.net/projects/mephisto/trunk@1408 567b1171-46fb-0310-a4c9-b4bef9110e78

---
+++ app/models/user_auth.rb
@@ -0,0 +1,55 @@
+require 'digest/sha1'
+class UserAuth < ActiveRecord::Base
+  set_table_name 'users'
+  self.abstract_class = true
+  # Virtual attribute for the unencrypted password
+  attr_accessor :password
+
+  validates_presence_of     :login, :email
+  validates_presence_of     :password,                   :if => :password_required?
+  validates_presence_of     :password_confirmation,      :if => :password_required?
+  validates_length_of       :password, :within => 5..40, :if => :password_required?
+  validates_confirmation_of :password,                   :if => :password_required?
+  validates_length_of       :login,    :within => 3..40
+  validates_length_of       :email,    :within => 3..100
+  validates_uniqueness_of   :login, :email, :salt
+  before_save :encrypt_password
+  
+  # Uncomment this to use activation
+  # before_create :make_activation_code
+
+  # Authenticates a user by their login name and unencrypted password.  Returns the user or nil.
+  def self.authenticate(login, password)
+    u = find_by_login(login) # need to get the salt
+    u.save and return u if u && u.authenticated?(password)
+  end
+
+  # Encrypts some data with the salt.
+  def self.encrypt(password, salt)
+    Digest::SHA1.hexdigest("--#{salt}--#{password}--")
+  end
+
+  def make_activation_code
+    self.activation_code = Digest::SHA1.hexdigest( Time.now.to_s.split('//').sort_by {rand}.join )
+  end
+
+  # Encrypts the password with the user salt
+  def encrypt(password)
+    self.class.encrypt(password, salt)
+  end
+
+  def authenticated?(password)
+    crypted_password == encrypt(password)
+  end
+
+  protected    
+    def encrypt_password
+      return if password.blank?
+      self.salt = Digest::SHA1.hexdigest("--#{Time.now.to_s}--#{login}--") if new_record?
+      self.crypted_password = encrypt(password)
+    end
+    
+    def password_required?
+      crypted_password.nil? || !password.blank?
+    end
+end
