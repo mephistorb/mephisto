dec8a2baab3390db0bdab50cfb3ba2899e48eb2e
Caged <Caged@567b1171-46fb-0310-a4c9-b4bef9110e78>
2006-07-27 05:22:20 +0000

    Remove user list from user show for now
    git-svn-id: http://svn.techno-weenie.net/projects/mephisto/trunk@1434 567b1171-46fb-0310-a4c9-b4bef9110e78

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,7 +3,7 @@
 # In the development environment your application's code is reloaded on
 # every request.  This slows down response time but is perfect for development
 # since you don't have to restart the webserver when you make code changes.
-config.cache_classes     = false
+config.cache_classes     = true
 
 # Log error messages when you accidentally call methods on nil.
 config.whiny_nils        = true
@@ -20,5 +20,5 @@ config.action_mailer.raise_delivery_errors = false
 
 config.after_initialize do
   require 'application' unless Object.const_defined?(:ApplicationController)
-  ReferencedPageCachingController.enabled = true
+  ReferencedPageCachingController.enabled = false
 end
