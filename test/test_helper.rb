f4c663351ce084ba280601b3a79625ed4e50b958
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2006-08-06 12:20:24 +0000

    move some code around for artificial boost in rake stats
    git-svn-id: http://svn.techno-weenie.net/projects/mephisto/trunk@1499 567b1171-46fb-0310-a4c9-b4bef9110e78

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@ ENV["RAILS_ENV"] = "test"
 ENV['TZ'] = 'US/Central'
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'test_help'
+require File.expand_path(File.dirname(__FILE__) + "/actor")
 
 Time.class_eval do
   class << self
@@ -21,7 +22,6 @@ Time.class_eval do
 end
 
 class Test::Unit::TestCase
-  include AuthenticatedTestHelper
   self.use_transactional_fixtures = true
   self.use_instantiated_fixtures  = false
 
@@ -68,6 +68,45 @@ class Test::Unit::TestCase
     assert_attachment_created 0 do
       yield
     end
+  end
+
+  # Sets the current user in the session from the user fixtures.
+  def login_as(user)
+    @request.session[:user] = user ? users(user).id : nil
+  end
+
+  def login_with_cookie_as(user)
+    @request.cookies['user'] = user ? CGI::Cookie.new( 
+      'name'    => 'user',
+      'value'   => users(user).activation_code,
+      'expires' => 2.weeks.from_now,
+      'path'    => '/',
+      'domain'  => 'example.com'
+    ) : nil
+  end
+
+  # Assert the block redirects to the login
+  # 
+  #   assert_requires_login(:bob) { get :edit, :id => 1 }
+  #
+  def assert_requires_login(user = nil, &block)
+    login_as(user) if user
+    block.call
+    assert_redirected_to :controller => 'account', :action => 'login'
+  end
+
+  # Assert the block accepts the login
+  # 
+  #   assert_accepts_login(:bob) { get :edit, :id => 1 }
+  #
+  # Accepts anonymous logins:
+  #
+  #   assert_accepts_login { get :list }
+  #
+  def assert_accepts_login(user = nil, &block)
+    login_as(user) if user
+    block.call
+    assert_response :success
   end
 
   def prepare_theme_fixtures
