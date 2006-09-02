414967ba196095c4d4214f349ae9ee758c17f268
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2006-09-02 01:36:33 +0000

    fix bug with <typo:code> macro
    git-svn-id: http://svn.techno-weenie.net/projects/mephisto/trunk@1976 567b1171-46fb-0310-a4c9-b4bef9110e78

--- vendor/plugins/filtered_column/lib/filtered_column/processor.rb
+++ vendor/plugins/filtered_column/lib/filtered_column/processor.rb
@@ -6,7 +6,7 @@ module FilteredColumn
   class Processor
     @@patterns = [
       /<(filter|macro|typo):([_a-zA-Z0-9]+)([^>]*)\/>/,
-      /<(filter|macro|typo):([_a-zA-Z0-9]+)([^>]*)>(.*?)<\/(filter|macro):([_a-zA-Z0-9]+)>/m
+      /<(filter|macro|typo):([_a-zA-Z0-9]+)([^>]*)>(.*?)<\/(filter|macro|typo):([_a-zA-Z0-9]+)>/m
       ].freeze
 
     def self.process_filter(filter_name, text)
