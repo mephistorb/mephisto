f9b5f11d8c5a588fc69338876c26e0726d5d40d2
Caged <Caged@567b1171-46fb-0310-a4c9-b4bef9110e78>
2006-09-08 21:50:50 +0000

    Did some tweaking on published_at_for in admin
    git-svn-id: http://svn.techno-weenie.net/projects/mephisto/trunk@2106 567b1171-46fb-0310-a4c9-b4bef9110e78

--- app/helpers/admin/articles_helper.rb
+++ app/helpers/admin/articles_helper.rb
@@ -16,7 +16,7 @@ module Admin::ArticlesHelper
 
   def published_at_for(article)
     return 'not published' unless article && article.published?
-    utc_to_local(article.published_at).to_s(article.published_at.year == Time.now.utc.year ? :plain : :standard)
+    utc_to_local(article.published_at).to_ordinalized_s(article.published_at.year == Time.now.utc.year ? :stub : :mdy)
   end
 
   def valid_filter?(filter = params[:filter])
