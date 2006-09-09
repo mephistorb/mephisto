c3df3b519d27c06b8e0968ba65b3b5f70f262908
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2006-09-09 01:20:31 +0000

    add migration for swept pages
    git-svn-id: http://svn.techno-weenie.net/projects/mephisto/trunk@2115 567b1171-46fb-0310-a4c9-b4bef9110e78

---
+++ db/migrate/056_move_to_scoped_cached_pages.rb
@@ -0,0 +1,24 @@
+class MoveToScopedCachedPages < ActiveRecord::Migration
+  def self.up
+    if Site.multi_sites_enabled
+      Site.find(:all).each do |site|
+        say_with_time "sweeping for #{site.title}..." do
+          expire_pages site.host, site.cached_pages
+        end
+      end
+    else
+      say_with_time "sweeping cached pages" do
+        expire_pages '', CachedPage.find(:all)
+      end
+    end
+  end
+
+  def self.down
+  end
+  
+  def self.expire_pages(prefix, pages)
+    pages.each do |page|
+      ActionController::Base.expire_page "/#{prefix}#{page.url}"
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -2,7 +2,7 @@
 # migrations feature of ActiveRecord to incrementally modify your database, and
 # then regenerate this schema definition.
 
-ActiveRecord::Schema.define(:version => 55) do
+ActiveRecord::Schema.define(:version => 56) do
 
   create_table "assets", :force => true do |t|
     t.column "content_type",     :string
