d4040a3eff06776fac616724139505df353e1465
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2006-09-09 01:09:40 +0000

    fix cached page controller
    git-svn-id: http://svn.techno-weenie.net/projects/mephisto/trunk@2113 567b1171-46fb-0310-a4c9-b4bef9110e78

--- app/controllers/admin/cached_pages_controller.rb
+++ app/controllers/admin/cached_pages_controller.rb
@@ -3,19 +3,23 @@ class Admin::CachedPagesController < Admin::BaseController
 
   def index
     CachedPage.with_current_scope do
-      @cached_page_pages, @cached_pages = paginate :cached_pages, :order => 'updated_at', :per_page => 30, 
-        :conditions => (params[:query] && ['url LIKE ?', ["#{params[:query]}%"]])
+      @cached_page_pages = Paginator.new self, site.cached_pages.count, 30, params[:page]
+      offset = (((params[:page] || 1).to_i - 1) * @cached_page_pages.items_per_page)
+      @cached_pages = site.cached_pages.find(:all, :order => 'updated_at', :limit => @cached_page_pages.items_per_page, :offset => offset,
+        :conditions => (params[:query] && ['url LIKE ?', ["#{params[:query]}%"]]))
     end
   end
   alias_method :query, :index
 
   def destroy
     page = site.cached_pages.find params[:id]
+    page.update_attribute :cleared_at, Time.now.utc
     self.class.expire_page page.url
   end
   
   def clear
     site.cached_pages.each { |p| self.class.expire_page(p.url) }
+    CachedPage.expire_pages site, site.cached_pages
     query
   end
   
