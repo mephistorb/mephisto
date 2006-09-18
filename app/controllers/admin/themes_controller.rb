9b7d905bdf636f720a1bb43ef4158189cefae97b
Caged <Caged@567b1171-46fb-0310-a4c9-b4bef9110e78>
2006-09-18 23:50:59 +0000

    Added basic theme interface.  Will change in the future
    git-svn-id: http://svn.techno-weenie.net/projects/mephisto/trunk@2211 567b1171-46fb-0310-a4c9-b4bef9110e78

---
+++ app/controllers/admin/themes_controller.rb
@@ -0,0 +1,8 @@
+class Admin::ThemesController < Admin::BaseController
+
+  def index
+  end
+
+  def import
+  end
+end
--- public/javascripts/mephisto/application.js
+++ public/javascripts/mephisto/application.js
@@ -456,6 +456,64 @@ SmartSearch.prototype = {
   }
 }
 
+
+ToolBox = Class.create();
+ToolBox.current = null;
+ToolBox.prototype = {      
+  initialize: function(element) {       
+    this.toolbox = $(element);
+    if(!this.toolbox) return;
+    this.timeout = null;
+    this.tools = this.findTools();
+    
+    Event.observe(this.toolbox, 'mouseover', this.onHover.bindAsEventListener(this), true);
+    Event.observe(this.toolbox, 'mouseout', this.onBlur.bindAsEventListener(this), true);
+    Event.observe(this.tools, 'mouseover', this.onHover.bindAsEventListener(this));
+    Event.observe(this.tools, 'mouseout', this.onBlur.bindAsEventListener(this));
+  },
+  
+  show: function() {
+    if(this.timeout) { 
+      clearTimeout(this.timeout); 
+      this.timeout = null;
+    }    
+    
+    if(ToolBox.current) {
+      ToolBox.current.hideTools();      
+    }
+    
+    if(this.tools) { 
+      Element.show(this.tools); 
+      ToolBox.current = this;
+    }    
+  },
+
+  onHover: function(event) {
+    this.show();
+  },
+
+  onBlur: function(event) {
+    this.considerHidingTools();
+  },
+
+  considerHidingTools: function() {
+    if(this.timeout) { clearTimeout(this.timeout); }
+    this.timeout = setTimeout(this.hideTools.bind(this), 500);
+  },
+
+  hideTools: function() {
+    clearTimeout(this.timeout);
+    this.timeout = null;
+    Element.hide(this.tools);          
+  },
+
+  findTools: function() { 
+    var tools = document.getElementsByClassName('tools', this.toolbox)[0];
+    if(!tools) { throw "You called new ToolBox() on an element which has no class=\"tools\" child element"; }
+    return tools;
+  }
+}
+
 Effect.DefaultOptions.duration = 0.25;
 Event.addBehavior({
   '#filesearch':     function() { window.spotlight = new Spotlight('filesearchform', 'filesearch'); },
@@ -475,6 +533,11 @@ Event.addBehavior({
   '#searchsubmit:click': function() { 
     $('published').value = '0';
     $('article-search').submit();
+  },
+  
+  '.theme': function() {
+    console.log(this);
+    new ToolBox(this);
   }
 });
 
