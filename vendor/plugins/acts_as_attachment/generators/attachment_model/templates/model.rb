0eec584c94c8737d3e1fb7c6092889f8a955f8bc
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2006-10-05 13:52:01 +0000

    removing plugins so they can be imported with piston
    git-svn-id: http://svn.techno-weenie.net/projects/mephisto/trunk@2327 567b1171-46fb-0310-a4c9-b4bef9110e78

--- vendor/plugins/acts_as_attachment/generators/attachment_model/templates/model.rb
+++
@@ -1,3 +0,0 @@
-class <%= class_name %> < ActiveRecord::Base
-  acts_as_attachment
-end
--- vendor/plugins/acts_as_versioned/test/fixtures/widget.rb
+++
@@ -1,4 +0,0 @@
-class Widget < ActiveRecord::Base
-  acts_as_versioned :sequence_name => 'widgets_seq'
-  non_versioned_columns << 'foo'
-end
