5d5e14d83485bfbe76161f1abeca969baa35cddb
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2006-11-07 06:46:24 +0000

    increment version
    git-svn-id: http://svn.techno-weenie.net/projects/mephisto/trunk@2441 567b1171-46fb-0310-a4c9-b4bef9110e78

--- lib/mephisto/version.rb
+++ lib/mephisto/version.rb
@@ -2,7 +2,7 @@ module Mephisto
   module Version
     MAJOR  = 0
     MINOR  = 7
-    TINY   = 0
+    TINY   = 2
     STRING = [MAJOR, MINOR, TINY].join('.').freeze
     TITLE  = "Noh-Varr".freeze
   end
