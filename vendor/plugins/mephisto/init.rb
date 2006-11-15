aaf4195c608fd882a25378aa809d9ca52c1675f3
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2006-11-15 03:29:02 +0000

    15 lines of pure evil
    git-svn-id: http://svn.techno-weenie.net/projects/mephisto/trunk@2471 567b1171-46fb-0310-a4c9-b4bef9110e78

--- vendor/plugins/mephisto/init.rb
+++ vendor/plugins/mephisto/init.rb
@@ -69,6 +69,35 @@ Module.class_eval do
       end
     EOS
   end
+
+  # A hash that maps Class names to an array of Modules to mix in when the class is instantiated.
+  @@class_mixins = {}
+  mattr_reader :class_mixins
+
+  # Specifies that this module should be included into the given classes when they are instantiated.
+  #
+  #   module FooMethods
+  #     include_into "Foo", "Bar"
+  #   end
+  def include_into(*klasses)
+    klasses.flatten!
+    klasses.each do |klass|
+      (@@class_mixins[klass] ||= []) << self
+      @@class_mixins[klass].uniq!
+    end
+  end
+end
+
+Class.class_eval do
+  # Instantiates a class and adds in any class_mixins that have been registered for it.
+  def inherited_with_mixins(klass)
+    returning inherited_without_mixins(klass) do |value|
+      mixins = @@class_mixins[klass.name]
+      klass.send(:include, *mixins) if mixins
+    end
+  end
+  
+  alias_method_chain :inherited, :mixins
 end
 
 # http://rails.techno-weenie.net/tip/2005/12/23/make_fixtures
