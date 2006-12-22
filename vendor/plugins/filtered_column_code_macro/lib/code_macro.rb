757d19e5ee8b0c7de51376e0e751d46a0ec89dbe
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2006-12-22 04:02:48 +0000

    expose more coderay options, add default options hash for customization
    git-svn-id: http://svn.techno-weenie.net/projects/mephisto/trunk@2565 567b1171-46fb-0310-a4c9-b4bef9110e78

--- vendor/plugins/filtered_column_code_macro/lib/code_macro.rb
+++ vendor/plugins/filtered_column_code_macro/lib/code_macro.rb
@@ -1,16 +1,31 @@
 require 'coderay'
 class CodeMacro < FilteredColumn::Macros::Base
+  DEFAULT_OPTIONS = {:css => :class, :wrap => :div, :line_numbers => :table, :tab_width => 2, :bold_every => 5, :hint => false, :line_number_start => 1}
   def self.filter(attributes, inner_text = '', text = '')
-    lang = attributes.delete(:lang)
-    attributes[:line_numbers] = :table unless attributes.has_key?(:line_numbers)
-    attributes.each do |key, value|
-      attributes[key] = value == 'nil' ? nil : value.to_sym rescue nil
-    end
+    # It's a whole lot easier to just set your attributes statically
+    # I think for most of us the only option we're gonna change is 'lang'
+    # refer to http://rd.cycnus.de/coderay/doc/classes/CodeRay/Encoders/HTML.html for more info
+    # use code_highlighter.css to change highlighting
+    # don't change, formats code so code_highlighter.css can be used
+
+    lang    = attributes[:lang].blank? ? nil : attributes[:lang].to_sym
+    options = DEFAULT_OPTIONS.dup
+    # type of line number to print options: :inline, :table, :list, nil [default = :table]
+    # you can change the line_numbers default value but you will probably have to change the css too
+    options[:line_numbers]      = attributes[:line_numbers].to_sym    unless attributes[:line_numbers].blank?
+    # changes tab spacing [default = 2]
+    options[:tab_width]         = attributes[:tab_width].to_i         unless attributes[:tab_width].blank?
+    # bolds every 'X' line number
+    options[:bold_every]        = attributes[:bold_every].to_i        unless attributes[:bold_every].blank?
+    # use it if you want to can be :info, :info_long, :debug just debugging info in the tags
+    options[:hint]              = attributes[:hint].to_sym            unless attributes[:hint].blank?
+    # start with line number
+    options[:line_number_start] = attributes[:line_number_start].to_i unless attributes[:line_number_start].blank?
 
     begin
-      CodeRay.scan(inner_text, lang.to_sym).html(attributes)
+      CodeRay.scan(inner_text, lang.to_sym).html(options)
     rescue ArgumentError
-      CodeRay.scan(inner_text, lang.to_sym).html(:line_numbers => :table)
+      CodeRay.scan(inner_text, lang.to_sym).html(DEFAULT_OPTIONS)
     rescue
       unless lang.blank?
         RAILS_DEFAULT_LOGGER.warn "CodeRay Error: #{$!.message}"
