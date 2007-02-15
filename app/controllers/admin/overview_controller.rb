827138938103a85800ee4645610d25bafb7f6975
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2007-02-15 08:03:14 +0000

    Fix admin overview date timezone.  [Ben Wyrosdick]
    git-svn-id: http://svn.techno-weenie.net/projects/mephisto/trunk@2741 567b1171-46fb-0310-a4c9-b4bef9110e78

--- app/controllers/admin/overview_controller.rb
+++ app/controllers/admin/overview_controller.rb
@@ -9,10 +9,10 @@ class Admin::OverviewController < Admin::BaseController
   def index
     @users = site.users(:order => 'updated_at desc')
     @events, @todays_events, @yesterdays_events = [], [], []
-    today, yesterday = Time.now.utc.to_date, 1.day.ago.utc.to_date
+    today, yesterday = utc_to_local(Time.now.utc).to_date, utc_to_local(1.day.ago.utc).to_date
     @articles = @site.unapproved_comments.count :all, :group => :article, :order => '1 desc'
     @site.events.find(:all, :order => 'events.created_at DESC', :include => [:article, :user], :limit => 50).each do |event|
-      event_date = event.created_at.to_date
+      event_date = utc_to_local(event.created_at).to_date
       if event_date >= today
         @todays_events
       elsif event_date == yesterday
