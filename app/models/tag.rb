f1f452cedb159e01353c5da6dcd2015d973a6189
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2007-11-24 05:00:41 +0000

    allow space AND comma delimited tags
    git-svn-id: http://svn.techno-weenie.net/projects/mephisto/trunk@3055 567b1171-46fb-0310-a4c9-b4bef9110e78

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,4 @@
 class Tag < ActiveRecord::Base
-  @@tag_parse_regex = /((?: |)['"]{0,1})['"]?\s*(.*?)\s*(?:[,'"]|$)(?:\1(?: |$))/
   has_many :taggings
 
   class << self
@@ -7,23 +6,19 @@ class Tag < ActiveRecord::Base
       find_by_name(tag.to_s)
     end
 
-    # parses a comma separated list of tags into tag names
-    # handles all kinds of different tags. (comma seperated, space seperated (in quotation marks))
-    # should handle most the common keyword formats.
-    #
-    # e.g.: b'log, emacs fun, rails, ruby => "b'log", "emacs fun", "rails", "ruby"
-    #       "b'log" "emacs fun" "rails" "ruby" => "b'log", "emacs fun", "rails", "ruby"
-    #       'b\'log' 'emacs fun' 'rails' 'ruby' => "b'log", "emacs fun", "rails", "ruby"
+    # parses a list of tags into tag names
     #
     #   Tag.parse('a, b, c')
     #   # => ['a', 'b', 'c']
+    #
+    #   Tag.parse("a b c")
+    #   # => ['a', 'b', 'c']
+    #
+    #   Tag.parse(%(a "b c"))
+    #   # => ['a', 'b c']
     def parse(list)
       return list if list.is_a?(Array)
-      returning list.scan(@@tag_parse_regex) do |tags|
-        tags.collect! { |t| t.last.strip!; t.last }
-        tags.uniq!
-        tags.delete_if &:blank?
-      end
+      list.include?(',') ? parse_with_commas(list) : parse_with_spaces(list)
     end
 
     # Parses comma separated tag list and returns tags for them.
@@ -44,6 +39,38 @@ class Tag < ActiveRecord::Base
         found_tags + (tag_names - found_tags.collect(&:name)).collect { |s| create!(:name => s) }
       end
     end
+  
+    private
+      def parse_with_commas(list)
+        cleanup_tags(list.split(','))
+      end
+      
+      def parse_with_spaces(list)
+        tags = []
+
+        # first, pull out the quoted tags
+        list.gsub!(/\"(.*?)\"\s*/ ) { tags << $1; "" }
+        
+        # then, get whatever's left
+        tags.concat list.split(/\s/)
+
+        cleanup_tags(tags)
+      end
+    
+      def cleanup_tags(tags)
+        tags.tap do |t|
+          t.collect! do |tag|
+            unless tag.blank?
+              tag.downcase!
+              tag.gsub!(/:/, '')
+              tag.strip!
+              tag
+            end
+          end
+          t.compact!
+          t.uniq!
+        end
+      end
   end
 
   def ==(comparison_object)
---
+++ spec/models/tag_spec.rb
@@ -0,0 +1,69 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+ModelStubbing.define_models :tags do
+  model Tag do
+    [:ruby, :rails, :mongrel, :plugin].each do |t|
+      stub t, :name => t.to_s
+    end
+  end
+end
+
+describe Tag, "#parse" do
+  define_models :tags
+
+  it "parses comma separated tags" do
+    Tag.parse('a,b,c').should  == %w(a b c)
+    Tag.parse('a, b c').should == %w(a b\ c)
+  end
+
+  it "parses simple tags" do
+    Tag.parse('"a" "b" "c"').should == %w(a b c)
+  end
+
+  it "parses space delimited tags" do
+    Tag.parse('a b c').should == %w(a b c)
+  end
+
+  it "parses tags with double quotes" do
+    Tag.parse('tagging, it"s, weirdness').should == %w(tagging it"s weirdness)
+  end
+  
+  it "parses tags with single quotes" do
+    Tag.parse('"tagging" "it\'s" "weirdness"').should == %w(tagging it's weirdness)
+  end
+  
+  it "parses tags with quotes and spaces" do
+    Tag.parse('"tagging" "it\'s weirdness"').should == %w(tagging it's\ weirdness)
+  end
+  
+  it "returns tag array" do
+    Tag.parse(%w(a b c)).should == %w(a b c)
+  end
+  
+  it "returns unique tags" do
+    ["a, b, b", %("a" "b" " b ")].each do |input|
+      Tag.parse(input).should == %w(a b)
+    end
+  end
+end
+
+describe Tag do
+  define_models :tags
+
+  it "finds or creates tags" do
+    lambda { Tag.find_or_create %w(ruby rails foo) }.should change { Tag.count }.by(1)
+  end
+  
+  it "creates tags from comma separated list" do
+    lambda { Tag.parse_to_tags 'ruby, a, b, rails, c' }.should change { Tag.count }.by(3)
+  end
+  
+  it "equals tags of the same name" do
+    tags(:ruby).should == 'ruby'
+    tags(:ruby).should == Tag.new(:name => "ruby")
+  end
+  
+  it "selects tags by name" do
+    Tag[:ruby].should == tags(:ruby)
+  end
+end
