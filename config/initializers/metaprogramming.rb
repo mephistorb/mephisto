08e831d1c9bf302524559c0cb019e9cd5ad0c33b
rick <technoweenie@gmail.com>
2008-02-03 10:02:38 -0800

    move Module.include_into into aaa so other plugins can use it

--- config/initializers/metaprogramming.rb
+++ config/initializers/metaprogramming.rb
@@ -13,42 +13,6 @@ Symbol.class_eval do
   end
 end
 
-Module.class_eval do
-  # A hash that maps Class names to an array of Modules to mix in when the class is instantiated.
-  @@class_mixins = {}
-  mattr_reader :class_mixins
-
-  # Specifies that this module should be included into the given classes when they are instantiated.
-  #
-  #   module FooMethods
-  #     include_into "Foo", "Bar"
-  #   end
-  def include_into(*klasses)
-    klasses.flatten!
-    klasses.each do |klass|
-      (@@class_mixins[klass] ||= []) << self
-      @@class_mixins[klass].uniq!
-    end
-  end
-  
-  # add any class mixins that have been registered for this class
-  def auto_include!
-    mixins = @@class_mixins[name]
-    send(:include, *mixins) if mixins
-  end
-end
-
-Class.class_eval do
-  # Instantiates a class and adds in any class_mixins that have been registered for it.
-  def inherited_with_mixins(klass)
-    returning inherited_without_mixins(klass) do |value|
-      klass.auto_include!
-    end
-  end
-  
-  alias_method_chain :inherited, :mixins
-end
-
 # http://rails.techno-weenie.net/tip/2005/12/23/make_fixtures
 ActiveRecord::Base.class_eval do
   # person.dom_id #-> "person-5"
--- vendor/plugins/aaa/init.rb
+++ vendor/plugins/aaa/init.rb
@@ -1,2 +1,38 @@
 # temporarily load this before all other plugins
 ::Object::RAILS_PATH = Pathname.new(File.expand_path(RAILS_ROOT))
+
+Module.class_eval do
+  # A hash that maps Class names to an array of Modules to mix in when the class is instantiated.
+  @@class_mixins = {}
+  mattr_reader :class_mixins
+
+  # Specifies that this module should be included into the given classes when they are instantiated.
+  #
+  #   module FooMethods
+  #     include_into "Foo", "Bar"
+  #   end
+  def include_into(*klasses)
+    klasses.flatten!
+    klasses.each do |klass|
+      (@@class_mixins[klass] ||= []) << name.to_s
+      @@class_mixins[klass].uniq!
+    end
+  end
+  
+  # add any class mixins that have been registered for this class
+  def auto_include!
+    mixins = @@class_mixins[name]
+    send(:include, *mixins.collect { |name| name.constantize }) if mixins
+  end
+end
+
+Class.class_eval do
+  # Instantiates a class and adds in any class_mixins that have been registered for it.
+  def inherited_with_mixins(klass)
+    returning inherited_without_mixins(klass) do |value|
+      klass.auto_include!
+    end
+  end
+  
+  alias_method_chain :inherited, :mixins
+end
