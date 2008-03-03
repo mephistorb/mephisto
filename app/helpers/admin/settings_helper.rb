e029e461363c853091c7498d614b2cc2d1640db4
François Beausoleil <francois@teksol.info>
2008-03-03 09:18:45 -0500

    Moved the DL to the individual spam detection engine settings templates.
    New SettingsHelper#spam_detection_settings_for to generate the correct block, including hiding the non-selected engine.

--- app/helpers/admin/settings_helper.rb
+++ app/helpers/admin/settings_helper.rb
@@ -1,2 +1,11 @@
 module Admin::SettingsHelper
+  def spam_engine_settings_for(klass_name)
+    if @site.spam_detection_engine == klass_name then
+      style = nil
+    else
+      style = "display:none"
+    end
+
+    content_tag(:div, klass_name.constantize.settings_template(@site), :id => File.basename(klass_name.underscore), :style => style)
+  end
 end
