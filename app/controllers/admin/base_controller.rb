45ffeb7a56d59b8abe0bd78b53cec861423c9654
rick <technoweenie@gmail.com>
2008-06-30 20:27:44 -0700

    security protection for admin actions

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -2,11 +2,24 @@ class Admin::BaseController < ApplicationController
   class_inheritable_reader :member_actions
   write_inheritable_attribute :member_actions, []
   include AuthenticatedSystem
+  before_filter :protect_action, :only => [:create, :update, :destroy]
   before_filter { |c| UserMailer.default_url_options[:host] = c.request.host_with_port }
   before_filter :login_from_cookie
   before_filter :login_required, :except => :feed
 
   protected
+    def protect_action
+      if request.get?
+        if params[:id]
+          redirect_to :action => 'show', :id => params[:id]
+        else
+          redirect_to :action => 'index'
+        end
+      else
+        true
+      end
+    end
+
     # standard authorization method.  allow logged in users that are admins, or members in certain actions
     def authorized?
       logged_in? && (admin? || member_actions.include?(action_name) || allow_member?)
