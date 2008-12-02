da8a69d407f5fff85600710e53ed45fd08fa128b
Eric Kidd <git@randomhacks.net>
2008-12-02 00:45:54 -0500

    Fix TRUNCATE TABLE errors in in-tree model_stubbing
    As discovered by Aubrey Holland, model_stubbing relies on the
    non-portable TRUNCATE TABLE command:
    
      http://groups.google.com/group/MephistoBlog/msg/7da91b7410271ff2
    
    Applying Aubrey's fix allows 'rake spec' to run against SQLite3.  Note
    that there are still a good number of spec failures which will need to
    be addressed.
    
    It's a little bit dodgy to patch the in-tree copy of model_stubbing,
    but doing so will suffice for now.

--- vendor/plugins/model_stubbing/lib/model_stubbing/model.rb
+++ vendor/plugins/model_stubbing/lib/model_stubbing/model.rb
@@ -85,7 +85,7 @@ module ModelStubbing
     end
     
     def purge
-      model_class.connection.execute "TRUNCATE TABLE #{connection.quote_column_name model_class.table_name}"
+      model_class.connection.delete "DELETE FROM #{connection.quote_column_name model_class.table_name}"
     end
     
     def connection
