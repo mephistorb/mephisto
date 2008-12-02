732676ef548972d6dcfd5c1b1882700e8774b9fd
Eric Kidd <git@randomhacks.net>
2008-12-01 23:18:14 -0500

    Do not assume asset IDs are assigned in a specific order
    This code assumed that after calling:
    
      Fixtures.delete_existing_fixtures_for(Asset.connection, :assets)
    
    ...the next asset created would be assigned ID 1.  But this isn't
    true with the SQLite database driver, which tries avoid reusing the
    IDs of deleted rows.  This patch fixes the test cases to look up rows
    using other techniques.  As a nice side-effect, we eliminate some
    code duplicated between two conditionally-executed tests.

--- test/functional/admin/assets_controller_upload_test.rb
+++ test/functional/admin/assets_controller_upload_test.rb
@@ -16,56 +16,39 @@ class Admin::AssetsControllerUploadTest < Test::Unit::TestCase
     Fixtures.delete_existing_fixtures_for(Asset.connection, :assets)
   end
 
-if has_image_processor?
   def test_should_sort_assets
+    assets_per_creation = has_image_processor? ? 3 : 1
+    created = []
     assert_difference sites(:first).assets, :count, 21 do
-      assert_difference Asset, :count, 63 do
+      assert_difference Asset, :count, 21 * assets_per_creation do
         t = 5.months.ago.utc
         21.times do |i|
           Time.mock! t + i.days do
-            sites(:first).assets.create(:title => "Asset for #{Time.now.to_s(:db)}", 
-                                        :uploaded_data => fixture_file_upload('assets/logo.png', 'image/png'))
+            created <<
+              sites(:first).assets.create(:title => "Asset for #{Time.now.to_s(:db)}", 
+                                          :uploaded_data => fixture_file_upload('assets/logo.png', 'image/png'))
           end
         end
       end
     end
+    created.reverse!
     get :index
     assert_response :success
-    assert_models_equal Asset.find(61, 58, 55, 52, :order => 'created_at desc'), assigns(:recent)
-    assert_models_equal Asset.find(*(((4..51).to_a << 1).in_groups_of(3).collect(&:first) << {:order => 'created_at desc'})), assigns(:assets)
+    assert_models_equal created[0..3], assigns(:recent)
+    assert_models_equal created[4..-1], assigns(:assets)
   end
-else
-  def test_should_sort_assets
-    old_count = Asset.count
-    assert_difference sites(:first).assets, :count, 21 do
-      assert_difference Asset, :count, 21 do
-        t = 5.months.ago.utc
-        21.times do |i|
-          Time.mock! t + i.days do
-            sites(:first).assets.create(:title => "Asset for #{Time.now.to_s(:db)}", 
-                                        :uploaded_data => fixture_file_upload('assets/logo.png', 'image/png'))
-          end
-        end
-      end
-    end
-    get :index
-    assert_response :success
-    assert_models_equal Asset.find(*((18..21).to_a.collect { |n| n + old_count } << {:order => 'created_at desc'})), assigns(:recent)
-    assert_models_equal Asset.find(*((01..17).to_a.collect { |n| n + old_count } << {:order => 'created_at desc'})), assigns(:assets)
-  end
-end
 
   def test_should_edit_asset
     login_as :quentin
     process_upload ['logo.png']
-    get :edit, :id => Asset.find(1).id
+    get :edit, :id => Asset.find(:first).id
     assert_response :success
   end
   
   def test_should_update_asset
     login_as :quentin
     process_upload ['logo.png']
-    post :update, :id => Asset.find(1).id, :asset => { :title => 'foo bar' }
+    post :update, :id => Asset.find(:first).id, :asset => { :title => 'foo bar' }
     assert_redirected_to assets_path
     assert_valid assigns(:asset)
     assert_equal 'foo bar', assigns(:asset).title
