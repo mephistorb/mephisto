4efb0c3ebd8eafd7f2c15aad843d135c05cbbd00
Eric Kidd <git@randomhacks.net>
2008-12-02 07:37:21 -0500

    Temporarily disable plugin specs
    As in 16335c092458b5fca79bd32a5d361f5424831802, these tests appear to
    rely on a missing plugin. I hope to re-enable these once we have a
    useable sample plugin in the source tree.
    
    Here are some links from Sven Fuchs explaining how to write new-style
    Mephisto plugins.
    
    http://jamescrisp.org/2008/08/25/migrating-mephisto-plugins-to-drax-08/
    http://groups.google.com/group/MephistoBlog/browse_thread/thread/ec7cc25e5720ec9c/8d8b867c8f020557?lnk=gst#8d8b867c8f020557
    http://groups.google.com/group/MephistoBlog/browse_thread/thread/7fd7d1198ef8345e/c000f0e1ea8c490e?lnk=gst#c000f0e1ea8c490e

--- spec/models/plugin_spec.rb
+++ spec/models/plugin_spec.rb
@@ -10,9 +10,12 @@ end
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Mephisto::Plugin do
-  def plugin_alpha
-    Engines.plugins['plugin_alpha']
-  end
+  # Temporarily disabled--see the comment on PluginWhammyJammy in
+  # test_helper.rb for an explanation.
+
+  #def plugin_alpha
+  #  Engines.plugins['plugin_alpha']
+  #end
   
   it "should filter plugin list and return mephisto plugins" do
     Engines.plugins.size.should > Mephisto.plugins.size
@@ -22,20 +25,20 @@ describe Mephisto::Plugin do
     Mephisto.plugins.each {|p| p.name.should =~ /^mephisto_/ }
   end
   
-  it "should be configurable when at least one option is defined" do
-    plugin_alpha.configurable?.should be_true
-    Engines.plugins['engines'].configurable?.should be_false
-  end
+  #it "should be configurable when at least one option is defined" do
+  #  plugin_alpha.configurable?.should be_true
+  #  Engines.plugins['engines'].configurable?.should be_false
+  #end
   
-  it "should allow to add tabs" do
-    Mephisto::Plugin.tabs.clear
-    plugin_alpha.add_tab :something
-    Mephisto::Plugin.tabs.first.first.should == :something
-  end
+  #it "should allow to add tabs" do
+  #  Mephisto::Plugin.tabs.clear
+  #  plugin_alpha.add_tab :something
+  #  Mephisto::Plugin.tabs.first.first.should == :something
+  #end
   
-  it "should allow to add admin tabs" do
-    Mephisto::Plugin.admin_tabs.clear
-    plugin_alpha.add_admin_tab :something
-    Mephisto::Plugin.admin_tabs.first.first.should == :something
-  end
+  #it "should allow to add admin tabs" do
+  #  Mephisto::Plugin.admin_tabs.clear
+  #  plugin_alpha.add_admin_tab :something
+  #  Mephisto::Plugin.admin_tabs.first.first.should == :something
+  #end
 end
