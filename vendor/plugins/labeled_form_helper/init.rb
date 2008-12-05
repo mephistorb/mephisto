4b8089af64c2d59d18483697ed7e5ea423221c82
Eric Kidd <git@randomhacks.net>
2008-12-04 21:23:13 -0500

    Remove labeled_form_helper, switch to standard one
    All these features are available in core.  We won't know if this patch
    is good until we actually get the website up, though.

--- vendor/plugins/labeled_form_helper/init.rb
+++
@@ -1,4 +0,0 @@
-require 'technoweenie/labeled_form_helper'
-ActionView::Base.send                 :include, Technoweenie::LabeledFormHelper
-ActionView::Helpers::InstanceTag.send :include, Technoweenie::LabeledInstanceTag
-ActionView::Helpers::FormBuilder.send :include, Technoweenie::FormBuilderMethods
