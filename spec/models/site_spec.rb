mephistorb/mephisto

commit 3414a37d9432f2a64ae3dda4238c6f546555c9ed

Changed TZInfo::TimeZone.all to ActiveSupport::Timezone.all =>  all tests and specs pass - but still need to fix routing errors when running application

app/views/admin/settings/index.rhtml
@@ -29,7 +29,7 @@
     </dt>
     <dd><%= f.text_field :email %></dd>
     <dt><label for="site_timezone">Website Timezone</label></dt>
-    <dd><%= select_tag 'site_timezone', options_for_select(['UTC'] + TZInfo::Timezone.all.map{|tz| [tz.to_s, tz.name]}.sort, @site.timezone.name), :name => 'site[timezone]' %></dd>
+    <dd><%= select_tag 'site_timezone', options_for_select(['UTC'] + ActiveSupport::TimeZone.all.map{|tz| [tz.to_s, tz.name]}.sort, @site.timezone.name), :name => 'site[timezone]' %></dd>
     <dt>
       <label for="site_lang">Site language</label>
       <span class="hint">Used to specify language in your site feeds</span>

commit 1189e41398778b7665a49338f6f063c4040b3472

Fix timezone bug caused by port to Rails 2.2
This bug was introduced in 3414a37d9432f2a64ae3dda4238c6f546555c9ed, and
was reported by "barontick" on #mephisto.  Changes introduced while
cleaning up unit test failures on the way to Rails 2.2 prevented users
from setting the site's timezone.

Some notes on this patch.
  1) We want to keep using 'tzinfo', and not Rail's built-in time zone
     classes, because the former supports daylight savings time.
  2) It's easier to just add a virtual timezone_name accessor instead of
     trying to do conversions in Site#timezone=.
  3) We can re-enable some old specs for Site, because there's no
     longer any danger of deleting site themes.

I really wish we had a better way of testing that HTML forms could be
submitted back to the database successfully, closing the loop between
two different sets of test cases on output and input.

app/models/site.rb
@@ -214,10 +214,13 @@ class Site < ActiveRecord::Base
   end
 
   composed_of :timezone, :class_name => 'TZInfo::Timezone', :mapping => %w(timezone name)
-  alias original_timezone_writer timezone=
-  def timezone=(name)
-    name = TZInfo::Timezone.new(name) unless name.is_a?(TZInfo::Timezone)
-    original_timezone_writer(name)
+
+  def timezone_name
+    timezone.name
+  end
+
+  def timezone_name= name
+    self.timezone = TZInfo::Timezone.new(name)
   end
 
   def page_cache_directory
@@ -281,7 +284,7 @@ class Site < ActiveRecord::Base
       self.search_path     = 'search' if search_path.blank?
       self.tag_path        = 'tags'   if tag_path.blank?
       [:permalink_style, :search_path, :tag_path].each { |a| send(a).downcase! }
-      self.timezone = 'UTC' if read_attribute(:timezone).blank?
+      self.timezone_name = 'UTC' if read_attribute(:timezone).blank?
       if new_record?
         self.approve_comments = false unless approve_comments?
         self.comment_age      = 30    unless comment_age

app/views/admin/settings/index.rhtml
@@ -29,7 +29,7 @@
     </dt>
     <dd><%= f.text_field :email %></dd>
     <dt><label for="site_timezone">Website Timezone</label></dt>
-    <dd><%= select_tag 'site_timezone', options_for_select(['UTC'] + ActiveSupport::TimeZone.all.map{|tz| [tz.to_s, tz.name]}.sort, @site.timezone.name), :name => 'site[timezone]' %></dd>
+    <dd><%= time_zone_select 'site', 'timezone_name', TZInfo::Timezone.all.sort, :model => TZInfo::Timezone %></dd>
     <dt>
       <label for="site_lang">Site language</label>
       <span class="hint">Used to specify language in your site feeds</span>

config/environment.rb
@@ -67,7 +67,7 @@ Rails::Initializer.run do |config|
   end
 
   # Allow table tags in untrusted HTML.
-  config.action_view.sanitized_allowed_tags = 'table', 'tr', 'td'
+  config.action_view.sanitized_allowed_tags = ['table', 'tr', 'td']
 
   # We're slowly moving the contents of vendor and vender/plugins into
   # vendor/gems by adding config.gem declarations.
@@ -81,6 +81,7 @@ Rails::Initializer.run do |config|
   config.gem 'will_paginate'
   config.gem 'mocha'
   config.gem 'coderay'
+  config.gem 'tzinfo'
 end
 
 # Don't update this file, make custom tweaks in config/initializers/custom.rb, 

spec/blueprints.rb
@@ -15,7 +15,7 @@ Site.blueprint do
   filter             { 'textile_filter' }
   approve_comments   { false }
   comment_age        { 30 }
-  timezone           { "America/New_York" }
+  timezone           { TZInfo::Timezone.new("America/New_York") }
   articles_per_page  { 15 }
   permalink_style    { ":year/:month/:day/:permalink" }
   tag_path           { 'tags' }

spec/models/site_spec.rb
@@ -1,11 +1,18 @@
-# TODO this seems to be deleting the site theme - please revisit
-# require File.dirname(__FILE__) + '/../spec_helper'
-# 
-# describe Site do
-#   it "should delete memberships on destruction" do
-#     @site = Site.make
-#     @membership = Membership.make(:site => @site)
-#     @site.memberships.should == [@membership]
-#     lambda { @site.destroy }.should change(Membership, :count).by(-1)
-#   end  
-# end
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe Site do
+  it "should delete memberships on destruction" do
+    @site = Site.make
+    @membership = Membership.make(:site => @site)
+    @site.memberships.should == [@membership]
+    lambda { @site.destroy }.should change(Membership, :count).by(-1)
+  end  
+
+  it "should have a timezone_name field" do
+    @site = Site.make
+    @site.timezone_name = "America/Montreal"
+    assert_equal "America/Montreal", @site.timezone_name
+    assert_equal TZInfo::Timezone.new("America/Montreal"), @site.timezone
+  end
+end
+