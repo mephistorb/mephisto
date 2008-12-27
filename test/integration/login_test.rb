cd63c144a6d63b008b4eb8995cc93f97721196d0
Eric Kidd <git@randomhacks.net>
2008-12-27 11:15:05 -0500

    Add integration test for "reset password"
    Note that we actually extract the activation URL from the e-mail and
    pass it directly to 'visit'.

--- test/integration/login_test.rb
+++ test/integration/login_test.rb
@@ -5,31 +5,56 @@ class LoginTest < ActionController::IntegrationTest
     @site = Site.make
     @user = User.make(:login => "sarah", :password => "password",
                       :admin => true, :password_confirmation => "password")
-    assert User.authenticate_for(@site, "sarah", "password")
+    visit "/admin"
   end
 
   test "should log in with valid username and password" do
-    visit "/admin"
     log_in_with "sarah", "password"
     assert_logged_in_as @user
   end
 
   test "should remember users when 'Remember me' is checked" do
-    visit "/admin"
     log_in_with "sarah", "password", :remember_me => true
     assert_logged_in_as @user, :remembered => true
   end
 
   test "should fail to log in with invalid user" do
-    visit "/admin"
     log_in_with "invalid", "password"
     assert_not_logged_in
+    assert_equal('Could not log you in. Are you sure your Login name ' +
+                 'and Password are correct?', flash[:error])
   end
 
   test "should fail to log in with invalid password" do
-    visit "/admin"
     log_in_with "sarah", "invalid"
     assert_not_logged_in
+  end
+
+  test "should reset password if given valid email" do
+    @emails = ActionMailer::Base.deliveries 
+    @emails.clear
+
+    assert_difference @emails, :size, 1 do
+      click_link "reset password"
+      fill_in "email", :with => @user.email
+      click_button "Reset"
+    end
+    assert_equal @user.email, @emails.first.to.first
+    assert_not_logged_in
+
+    # Extract the activation URL from the e-mail.
+    @emails.first.body =~ %r{(http://[^ ]*)}
+    activation_url = $1
+    assert_not_nil activation_url
+
+    visit activation_url
+    assert_logged_in_as @user
+
+    fill_in "Password", :with => "newpass"
+    fill_in "Password confirmation", :with => "newpass"
+    click_button "Save my profile"
+
+    assert User.authenticate_for(@site, @user.login, "newpass")
   end
 
   def log_in_with login, password, options={}
